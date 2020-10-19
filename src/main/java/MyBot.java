69634a1ba5c626e4bd5f804f44eced444e298cb6
toriningenn <annamihailova72@yandex.com>
2020-10-19 23:06:23 +0200

    ypa

---
+++ src/main/java/MyBot.java
@@ -0,0 +1,29 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
+
+public class MyBot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                .setChatId(update.getMessage().getChatId())
+                .setText(update.getMessage().getForwardFrom().getFirstName());
+        try {
+            execute(message); // Call method to send the message
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public String getBotUsername() {
+        return "getthatusernamebot";
+    }
+
+    public String getBotToken() {
+        return token;
+    }
+}
