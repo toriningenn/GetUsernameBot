679d2fc8b76dda70bb6e668ee127bbe04f7109ea
toriningenn <annamihailova72@yandex.com>
2020-10-23 20:05:51 +0200

    cbt

--- src/main/java/MyBot.java
+++ src/main/java/MyBot.java
@@ -24,6 +24,6 @@ public class MyBot extends TelegramLongPollingBot {
     }
 
     public String getBotToken() {
-        return System.getenv("TOKEN");
+        return System.getenv().get("TOKEN");
     }
 }
